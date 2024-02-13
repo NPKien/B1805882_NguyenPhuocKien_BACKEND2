b1efd37d8521b87a6c4eb84f56eb8497c5fee084
Npkien <npkien2912@gmail.com>
2024-02-13 16:59:47 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const contactsRouter = require("./app/routes/contact.route");
 
 app.get('/', (req, res) => {
   res.json('Welcome to contact book application');
 });
+
+app.use('/api/contacts', contactsRouter);
 
 module.exports = app;
